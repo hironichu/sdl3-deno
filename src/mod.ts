06abfab3030bff7f9399d13c816a1cc982a2d598
g9wp <g9vvp@outlook.com>
2025-04-19 16:23:18 +0800

    do not export ttf in main mod

--- src/mod.ts
+++ src/mod.ts
@@ -2,8 +2,8 @@ import * as SDL from "../gen/SDL.ts";
 export { SDL }
 import * as IMG from "../gen/IMG.ts";
 export { IMG }
-import * as TTF from "../gen/TTF.ts";
-export { TTF }
+// import * as TTF from "../gen/TTF.ts";
+// export { TTF }
 
 export { Tray } from "./tray.ts";
 
@@ -18,8 +18,8 @@ export { Event, EventType } from "./events.ts";
 export { openUrl } from "./misc.ts"
 
 
-export type { Render, RenderDriver, Texture } from "./render.ts";
+export { Render, RenderDriver, Texture } from "./render.ts";
 
-export type { Window, VideoDriver, DisplayModePtr } from "./video.ts";
+export { Window, VideoDriver, DisplayModePtr } from "./video.ts";
 
-export type { Properties, PropertyType } from "./properties.ts"
+export { Properties, PropertyType } from "./properties.ts"
